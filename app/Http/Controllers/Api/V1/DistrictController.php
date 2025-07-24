72b7435bbdebe224db5cf2c4b094a4668937f3fb
lulzshadowwalker <lulzshadowwalker@gmail.com>
2025-07-24 15:55:08 +0300

    Add DistrictController and DistrictResource with routes and tests for district management

---
+++ app/Http/Controllers/Api/V1/DistrictController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\V1\DistrictResource;
+use App\Models\District;
+
+class DistrictController extends Controller
+{
+    public function index()
+    {
+        $districts = District::all();
+        return DistrictResource::collection($districts);
+    }
+
+    public function show(District $district)
+    {
+        return new DistrictResource($district);
+    }
+}
