4706b1ce3e840fa7f75b8cf774e0c32b364bbb1e
lulzshadowwalker <lulzshadowwalker@gmail.com>
2025-09-02 20:20:59 +0300

    Comment out admin notification logic in MessageObserver

--- app/Observers/MessageObserver.php
+++ app/Observers/MessageObserver.php
@@ -13,16 +13,16 @@ class MessageObserver
     public function created(Message $message): void
     {
         MessageSent::dispatch($message);
-        if ($message->user->isAdmin) {
-            $message->chatRoom->participants()->whereHas('roles', function (Builder $query) {
-                $query->where('name', '!=', Role::Admin->value);
-            })
-                ->get()
-                ->each(function ($user) use ($message) {
-                    //  TODO: Write a feature test for notifications
-                    $user->notify(new SupportChatMessageNotification($message));
-                });
-        }
+        // if ($message->user->isAdmin) {
+        //     $message->chatRoom->participants()->whereHas('roles', function (Builder $query) {
+        //         $query->where('name', '!=', Role::Admin->value);
+        //     })
+        //         ->get()
+        //         ->each(function ($user) use ($message) {
+        //             //  TODO: Write a feature test for notifications
+        //             $user->notify(new SupportChatMessageNotification($message));
+        //         });
+        // }
 
         // manually calling touch to update the chat room's updated_at timestamp
         // because it seems that simply defining $touches in the model does not work as expected
