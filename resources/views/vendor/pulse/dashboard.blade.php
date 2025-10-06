7f87ca4fab0e7c7804fbe577c941f92987965686
lulzshadowwalker <lulzshadowwalker@gmail.com>
2025-10-06 19:18:55 +0300

    feat: Refactor dashboard layout and organize components for better readability

--- resources/views/vendor/pulse/dashboard.blade.php
+++ resources/views/vendor/pulse/dashboard.blade.php
@@ -1,35 +1,32 @@
 <x-pulse>
+    {{-- Server & App Status --}}
     <livewire:pulse.servers cols="full" />
-
     <livewire:pulse.usage cols="4" rows="2" />
+    <livewire:pulse.usage-hours cols="4" />
 
+    {{-- System Performance --}}
     <livewire:pulse.queues cols="4" />
-
     <livewire:pulse.cache cols="4" />
+    <livewire:pulse.log-files cols="4" />
 
+    {{-- Real-time Activity --}}
     <livewire:reverb.connections cols="full" />
-
     <livewire:reverb.messages cols="full" />
 
-    <livewire:pulse.slow-queries cols="full" />
-
-    <livewire:pulse.exceptions cols="6" />
-
-    <livewire:pulse.slow-requests cols="6" />
-
-    <livewire:pulse.slow-jobs cols="6" />
-
-    <livewire:pulse.slow-outgoing-requests cols="6" />
-
-    <livewire:pulse.about-application />
-
+    {{-- Request Monitoring --}}
     <livewire:requests-graph cols="6" />
+    <livewire:requests cols="6" />
 
-    <livewire:requests cols="8" />
+    {{-- Performance Issues --}}
+    <livewire:pulse.slow-queries cols="full" />
+    <livewire:pulse.slow-requests cols="4" />
+    <livewire:pulse.slow-jobs cols="4" />
+    <livewire:pulse.slow-outgoing-requests cols="4" />
 
-    <livewire:pulse.usage-hours cols='4' />
+    {{-- Issues & Errors --}}
+    <livewire:pulse.exceptions cols="6" />
+    <livewire:pulse.validation-errors cols="6" />
 
-    <livewire:pulse.validation-errors cols="8" rows="4" />
-
-    <livewire:pulse.log-files cols="4" />
+    {{-- Additional Info --}}
+    <livewire:pulse.about-application cols="full" />
 </x-pulse>
